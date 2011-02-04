5be249326fc32285aa158a88471b1e41f2129882
pR0Ps <pR0Ps.CM@gmail.com>
2011-02-03 23:28:08 -0500

    Got the guy walking around at the whim of the player

--- Assets/Player/PlayerDefenderController.js
+++ Assets/Player/PlayerDefenderController.js
@@ -1,5 +1,5 @@
 function Update () {
-   if (Input.GetAxis("Vertical") > 0) {
+   if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) {
        animation.CrossFade ("run");
    }
    else {
